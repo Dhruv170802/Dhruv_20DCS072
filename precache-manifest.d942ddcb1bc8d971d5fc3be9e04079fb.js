820c2dc7b07e1cc87f6df6c0d6c629acee00509c
Dhruv170802 <113326281+Dhruv170802@users.noreply.github.com>
2022-09-12 10:28:56 +0530

    Updates

---
+++ precache-manifest.d942ddcb1bc8d971d5fc3be9e04079fb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "338f6912a07f6cb903b9ce64d1bab944",
+    "url": "/Dhruv_20DCS072/index.html"
+  },
+  {
+    "revision": "5204efd19cd4bb25612c",
+    "url": "/Dhruv_20DCS072/static/css/2.a1840750.chunk.css"
+  },
+  {
+    "revision": "f44e9a53aefbe95abace",
+    "url": "/Dhruv_20DCS072/static/css/main.6777df22.chunk.css"
+  },
+  {
+    "revision": "5204efd19cd4bb25612c",
+    "url": "/Dhruv_20DCS072/static/js/2.29c5b276.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/Dhruv_20DCS072/static/js/2.29c5b276.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f44e9a53aefbe95abace",
+    "url": "/Dhruv_20DCS072/static/js/main.0c94c0a2.chunk.js"
+  },
+  {
+    "revision": "250b6ddb3bdb14911e2b",
+    "url": "/Dhruv_20DCS072/static/js/runtime-main.df662ff8.js"
+  }
+]);
